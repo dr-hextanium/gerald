33cb8621519866fe3c88c65bf6239ecd048e7d68
dr-hextanium <hextanium.on.discord@gmail.com>
2025-02-04 19:25:23 -0500

    add: claw go brrr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystem/Claw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystem/Claw.kt
@@ -1,3 +1,26 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystem
 
-class Claw
+import com.qualcomm.robotcore.hardware.Servo
+import org.firstinspires.ftc.teamcode.hardware.wrapper.useful.Bound
+
+class Claw(val servo: Servo, val bound: Bound) : ISubsystem {
+    var position = 0.0
+
+    override fun reset() {
+        servo.scaleRange(bound.lower, bound.upper)
+    }
+
+    override fun read() { }
+
+    override fun update() { }
+
+    override fun write() { servo.position = position.coerceIn(0.0, 1.0) }
+
+    fun open() { position = OPEN }
+    fun close() { position = CLOSED }
+
+    companion object {
+        const val OPEN = 0.0
+        const val CLOSED = 1.0
+    }
+}
