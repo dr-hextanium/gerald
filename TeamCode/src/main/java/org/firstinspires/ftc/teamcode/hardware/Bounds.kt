393f5f6ba8513ec80316605aa4d05c89e84aff24
dr-hextanium <hextanium.on.discord@gmail.com>
2025-02-07 21:41:27 -0500

    add: example bounds to fill out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Bounds.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Bounds.kt
@@ -9,4 +9,45 @@ object Bounds {
         Bound(0.0, 1.0),
         Bound(0.deg, 270.deg),
     )
+
+    object Arm {
+        val left = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+
+        val right = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+    }
+
+    object Diffy {
+        val left = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+
+        val right = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+    }
+
+    object Deposit {
+        val left = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+
+        val right = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+
+        val pivot = UsefulServoRange(
+            Bound(0.0, 1.0),
+            Bound(0.deg, 355.deg),
+        )
+    }
 }
